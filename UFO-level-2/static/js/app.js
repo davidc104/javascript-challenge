81e0683c1effda0252d1bfad6cd506fc86cc7f32
David Chao <dchao104@gmail.com>
2019-10-26 00:32:03 -0500

    Upload UFO-level-2: Multiple Search Categories

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,72 @@
+// From data.js
+var tableData = data;
+//console.log(tableData);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Show the UFO records in the table with id="ufo-table"
+function loadTable(dataRows) {
+    // Loop through the records to show each vaule in order
+    dataRows.forEach(function (ufoReport) {
+        console.log(ufoReport);
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(function ([key, value]) {
+            console.log(key, value);
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+// Inititalize the webpage
+loadTable(tableData);
+
+// The Submit Date button is clicked
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function () {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clean up the table content
+    tbody.html("");
+
+    var inputSelect = d3.select("#select_opt");
+    var inputOption = inputSelect.property("value");
+    console.log("Option is:");
+    console.log(inputOption);
+
+    // user input as variable
+    var inputDate = d3.select("#select_text");
+    var inputValue = inputDate.property("value");
+    console.log("input is:");
+    console.log(inputValue);
+
+    // Filter Data
+
+    var filteredData;
+
+    switch (inputOption) {
+        case "date":
+            filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+            break;
+        case "city":
+            filteredData = tableData.filter(tableData => tableData.city === inputValue);
+            break;
+        case "state":
+            filteredData = tableData.filter(tableData => tableData.state === inputValue);
+            break;
+        case "country":
+            filteredData = tableData.filter(tableData => tableData.country === inputValue);
+            break;
+        case "shape":
+            filteredData = tableData.filter(tableData => tableData.shape === inputValue);
+            break;
+        default:
+        // code block
+    }
+
+    // Call loadTable Function to show the selected UFO records
+    loadTable(filteredData);
+});
